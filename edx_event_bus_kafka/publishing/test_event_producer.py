a9c6ac72afbf66399da6abb01182ab466123c60b
Tim McCormack <tmccormack@edx.org>
2022-08-26 15:14:50 +0000

    test: Lift producer test data to be instance variables

--- edx_event_bus_kafka/publishing/test_event_producer.py
+++ edx_event_bus_kafka/publishing/test_event_producer.py
@@ -24,8 +24,10 @@ except ImportError:  # pragma: no cover
 class TestEventProducer(TestCase):
     """Test producer."""
 
-    def test_extract_event_key(self):
-        event_data = {
+    def setUp(self):
+        super().setUp()
+        self.signal = openedx_events.learning.signals.SESSION_LOGIN_COMPLETED
+        self.event_data = {
             'user': UserData(
                 id=123,
                 is_active=True,
@@ -37,17 +39,17 @@ class TestEventProducer(TestCase):
             )
         }
 
-        assert ep.extract_event_key(event_data, 'user.pii.username') == 'foobob'
+    def test_extract_event_key(self):
+        assert ep.extract_event_key(self.event_data, 'user.pii.username') == 'foobob'
         with pytest.raises(Exception,
                            match="Could not extract key from event; lookup in xxx failed at 'xxx' in dictionary"):
-            ep.extract_event_key(event_data, 'xxx')
+            ep.extract_event_key(self.event_data, 'xxx')
         with pytest.raises(Exception,
                            match="Could not extract key from event; lookup in user.xxx failed at 'xxx' in object"):
-            ep.extract_event_key(event_data, 'user.xxx')
+            ep.extract_event_key(self.event_data, 'user.xxx')
 
     def test_descend_avro_schema(self):
-        signal = openedx_events.learning.signals.SESSION_LOGIN_COMPLETED
-        schema = AvroSignalSerializer(signal).schema
+        schema = AvroSignalSerializer(self.signal).schema
 
         assert ep.descend_avro_schema(schema, ['user', 'pii', 'username']) == {"name": "username", "type": "string"}
 
@@ -57,8 +59,7 @@ class TestEventProducer(TestCase):
         assert isinstance(excinfo.value.__cause__, IndexError)
 
     def test_extract_key_schema(self):
-        signal = openedx_events.learning.signals.SESSION_LOGIN_COMPLETED
-        schema = ep.extract_key_schema(AvroSignalSerializer(signal), 'user.pii.username')
+        schema = ep.extract_key_schema(AvroSignalSerializer(self.signal), 'user.pii.username')
         assert schema == '{"name": "username", "type": "string"}'
 
     def test_get_producer_for_signal_unconfigured(self):
@@ -100,25 +101,12 @@ class TestEventProducer(TestCase):
         )
 
     def test_send_to_event_bus(self):
-        signal = openedx_events.learning.signals.SESSION_LOGIN_COMPLETED
-        event_data = {
-            'user': UserData(
-                id=123,
-                is_active=True,
-                pii=UserPersonalData(
-                    username='foobob',
-                    email='bob@foo.example',
-                    name="Bob Foo",
-                )
-            )
-        }
-
         mock_producer = MagicMock()
         with patch('edx_event_bus_kafka.publishing.event_producer.get_producer_for_signal', return_value=mock_producer):
-            ep.send_to_event_bus(signal, 'user_stuff', 'user.id', event_data)
+            ep.send_to_event_bus(self.signal, 'user_stuff', 'user.id', self.event_data)
 
         mock_producer.produce.assert_called_once_with(
-            'user_stuff', key=123, value=event_data,
+            'user_stuff', key=123, value=self.event_data,
             on_delivery=ep.on_event_deliver,
             headers={'ce_type': 'org.openedx.learning.auth.session.login.completed.v1'},
         )
