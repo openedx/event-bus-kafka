25649eefdac2a01c043cf8891da3342c44eced4a
Tim McCormack <tmccormack@edx.org>
2022-09-01 13:11:28 +0000

    Link to ADR in main modules.

--- edx_event_bus_kafka/__init__.py
+++ edx_event_bus_kafka/__init__.py
@@ -1,5 +1,9 @@
 """
 Kafka implementation for Open edX event bus.
+
+Public API will be in this module for the most part.
+
+See ADR ``docs/decisions/0006-public-api-and-app-organization.rst`` for the reasoning.
 """
 
 from edx_event_bus_kafka.internal.producer import EventProducerKafka, get_producer
