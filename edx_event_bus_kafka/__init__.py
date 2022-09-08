7d5991ce5b1abfaf1a82d5c9b467253a3485a10d
Rebecca Graber <rgraber@edx.org>
2022-09-08 16:02:15 -0400

    fixup!: isort

--- edx_event_bus_kafka/__init__.py
+++ edx_event_bus_kafka/__init__.py
@@ -6,7 +6,7 @@ Public API will be in this module for the most part.
 See ADR ``docs/decisions/0006-public-api-and-app-organization.rst`` for the reasoning.
 """
 
-from edx_event_bus_kafka.internal.producer import EventProducerKafka, get_producer
 from edx_event_bus_kafka.internal.consumer import KafkaEventConsumer
+from edx_event_bus_kafka.internal.producer import EventProducerKafka, get_producer
 
 __version__ = '0.6.3'
