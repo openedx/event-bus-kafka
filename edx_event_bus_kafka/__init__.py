dc05b9a8da80edfc1e7ef325de64c2fd31c9572a
Rebecca Graber <rgraber@edx.org>
2022-09-09 08:26:52 -0400

    fixup!: version

--- edx_event_bus_kafka/__init__.py
+++ edx_event_bus_kafka/__init__.py
@@ -9,4 +9,4 @@ See ADR ``docs/decisions/0006-public-api-and-app-organization.rst`` for the reas
 from edx_event_bus_kafka.internal.consumer import KafkaEventConsumer
 from edx_event_bus_kafka.internal.producer import EventProducerKafka, get_producer
 
-__version__ = '0.6.3'
+__version__ = '0.7.0'
