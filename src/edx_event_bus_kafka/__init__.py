b8b5fb546fd514275ba8205ae571a3e678b4335d
irfanuddinahmad <34648393+irfanuddinahmad@users.noreply.github.com>
2026-09-09 14:07:35 +0500

    fix: remove unnecessary try/except around __version__ (not present pre-migration)

--- src/edx_event_bus_kafka/__init__.py
+++ src/edx_event_bus_kafka/__init__.py
@@ -6,13 +6,9 @@ Public API will be in this module for the most part.
 See ADR ``docs/decisions/0006-public-api-and-app-organization.rst`` for the reasoning.
 """
 
-from importlib.metadata import PackageNotFoundError
 from importlib.metadata import version as get_version
 
 from edx_event_bus_kafka.internal.consumer import KafkaEventConsumer
 from edx_event_bus_kafka.internal.producer import KafkaEventProducer, create_producer
 
-try:
-    __version__ = get_version('edx-event-bus-kafka')
-except PackageNotFoundError:  # pragma: no cover
-    pass
+__version__ = get_version('edx-event-bus-kafka')
